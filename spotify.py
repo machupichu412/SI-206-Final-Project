8711d0f65deee4e76817c7460c05813db3b06444
machupichu412 <mattyeh@umich.edu>
2022-12-06 11:11:30 -0500

    test

---
+++ spotify.py
@@ -0,0 +1,26 @@
+import pprint
+import sys
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+scope = "user-library-read"
+cid = "f82a1ef57e2b4bab94afa5fc9f146d0f"
+secret = "bdb623d5ad514a5c9208d26b578479e9"
+
+
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+artist_name = []
+track_name = []
+popularity = []
+track_id = []
+track_results = sp.search(q='year:2018', type='track', limit=25)
+for i, t in enumerate(track_results['tracks']['items']):
+        artist_name.append(t['artists'][0]['name'])
+        track_name.append(t['name'])
+        track_id.append(t['id'])
+        popularity.append(t['popularity'])
+
+print(len(artist_name))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+from spotipy.oauth2 import SpotifyOAuth
