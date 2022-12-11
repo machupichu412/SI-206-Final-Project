8e450393bb5586e732f9137c70cdd3f176ff7279
machupichu412 <mattyeh@umich.edu>
2022-12-11 17:32:39 -0500

    changed celebrityninjas api to api ninjas api

--- celebrities.py
+++ celebrities.py
@@ -1,12 +1,18 @@
 import requests
+import sqlite3 as sql
 
-celebrities = []
+def get_artist_info(cur, conn):
+    cur.execute ("SELECT name FROM artists")
+    artist_list = cur.fetchall()
+    artists = [x[0] for x in artist_list]
+    return artists
 
-for i in celebrities:
-    api_url = 'https://api.celebrityninjas.com/v1/search?name='
-    name = "Beyonce"
-    response = requests.get(api_url + name, headers={'X-Api-Key': 'QU/Mur5QkwH5le8q3vJXAw==DMX06hqjhQmyHZ4T'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
-    else:
-        print("Error:", response.status_code, response.text)
+def update_artist_table(artists, cur, conn):
+    for i in artists:
+        name = i
+        api_url = 'https://api.api-ninjas.com/v1/celebrity?name={}'.format(name)
+        response = requests.get(api_url, headers={'X-Api-Key': 'QU/Mur5QkwH5le8q3vJXAw==ZePF0pHAnt3Lx2nO'})
+        if response.status_code == requests.codes.ok:
+            print(response.text)
+        else:
+            print("Error:", response.status_code, response.text)
