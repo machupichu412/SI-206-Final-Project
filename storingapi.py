94cf5365c20ce13feca198808c3d8bb4a18428c9
Will Clancy <112428497+willjclancy@users.noreply.github.com>
2022-12-12 11:09:47 -0500

    Add files via upload

--- storingapi.py
+++ storingapi.py
@@ -11,12 +11,12 @@ def setUpDatabase(db_name):
 
 def create_tastedive_table(cur, conn):
     cur.execute("DROP TABLE IF EXISTS tastedive")
-    cur.execute("CREATE TABLE tastedive (artist_id INTEGER PRIMARY KEY, similar_artist_id INTEGER, media_type TEXT)")
+    cur.execute("CREATE TABLE tastedive (artist_id INTEGER, similar_artist_id INTEGER, media_type TEXT)")
     conn.commit()
 
 def create_artists_table(cur, conn):
     cur.execute("DROP TABLE IF EXISTS artists")
-    cur.execute("CREATE TABLE artists (artist_id INTEGER PRIMARY KEY, name TEXT UNIQUE, age INTEGER, net_worth INTEGER)")
+    cur.execute("CREATE TABLE artists (artist_id INTEGER PRIMARY KEY, name TEXT, age INTEGER, net_worth INTEGER)")
     conn.commit()
 
 def create_spotify_table(cur, conn):
@@ -26,68 +26,10 @@ def create_spotify_table(cur, conn):
 
 def create_genre_table(cur, conn):
     cur.execute("DROP TABLE IF EXISTS genre")
-    cur.execute("CREATE TABLE genre (genre_id INTEGER PRIMARY KEY, genre_name TEXT UNIQUE)")
+    cur.execute("CREATE TABLE genre (genre_id INTEGER PRIMARY KEY, genre_name TEXT)")
     conn.commit()
 
 
-
-
-
-
-'''
-def create_employee_table(cur, conn):
-    cur.execute("DROP TABLE IF EXISTS Employees")
-    cur.execute("CREATE TABLE Employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, hire_date TEXT,job_id INTEGER, salary INTEGER)")
-    conn.commit()
-
-# ADD EMPLOYEE'S INFORMTION TO THE TABLE
-
-def add_employee(filename, cur, conn):
-    #load .json file and read job data
-    # WE GAVE YOU THIS TO READ IN DATA
-    f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), filename)))
-    file_data = f.read()
-    f.close()
-    # THE REST IS UP TO YOU
-    json_data = json.loads(file_data)
-    for item in json_data:
-        emp_id = int(item['employee_id'])
-        first = item['first_name']
-        last = item['last_name']
-        hire = item['hire_date']
-        job_id = int(item['job_id'])
-        salary = int(item['salary'])
-        cur.execute("INSERT OR IGNORE INTO Employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES(?,?,?,?,?,?)", (emp_id, first, last, hire, job_id, salary))
-    conn.commit()
-
-# TASK 2: GET JOB AND HIRE_DATE INFORMATION
-def job_and_hire_date(cur, conn):
-    pass
-
-# TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
-# Apply JOIN clause to match individual employees
-def problematic_salary(cur, conn):
-    pass
-
-# TASK 4: VISUALIZATION
-def visualization_salary_data(cur, conn):
-    cur.execute("SELECT Employees.salary, Jobs.job_title FROM Employees JOIN Jobs on Employees.job_id = Jobs.job_id")
-    salary_job_list = cur.fetchall()
-    job_list = []
-    salary_list = []
-    for item in salary_job_list:
-        job_list.append(item[1])
-        salary_list.append(item[0])
-
-    plt.figure()
-    plt.scatter(x = job_list, y = salary_list)
-
-    cur.execute("SELECT min_salary, max_salary, job_title FROM Jobs")
-    
-    plt.xticks(rotation = 45)
-    plt.tight_layout()
-    plt.show()
-'''
 
 def main():
     cur, conn = setUpDatabase("junkies.db")
--- tastedive.py
+++ tastedive.py
@@ -1,10 +1,13 @@
 import requests
 import json
+import sqlite3 as sql
 
 accesskey = "445402-SpotifyJ-NH86KIWN"
 
-def get_artist_from_tastedive(name):
+def get_artist_from_tastedive(name, cur, conn):
     url = 'https://tastedive.com/api/similar'
+    accesskey = "445402-SpotifyJ-NH86KIWN"
+
     param = {}
 
     param['q'] = name
@@ -14,10 +17,31 @@ def get_artist_from_tastedive(name):
 
     r = requests.get(url,params=param)
     js = json.loads(r.text)
-    return js
+    cur.execute("SELECT artist_id FROM artists WHERE name = ?", (name, ))
+    primary_artist_id = cur.fetchone()[0]
+    conn.commit()
+    for items in js.values():
+        results = items['Results']
+        for i in results:
+            artist_name = i['Name']
+            #cur.execute("SELECT artist_id FROM artists WHERE name = ?" (name, ))
+            #inlist = cur.fetchall()
+            #print(len(inlist))
+            cur.execute("INSERT OR IGNORE INTO artists (name) VALUES (?)", (artist_name, ))
+            conn.commit()
+            cur.execute("SELECT artist_id FROM artists WHERE name = ?", (artist_name, ))
+            artist_id = cur.fetchone()[0]
+            conn.commit()
+            art_type = i['Type']
+            cur.execute("INSERT OR IGNORE INTO tastedive (artist_id, similar_artist_id, media_type) VALUES (?,?,?)", (primary_artist_id, artist_id, art_type))
+            conn.commit()
+            
+    
 
 def main():
-    print(get_artist_from_tastedive("Drake"))
+    conn = sql.connect("junkies.db")
+    cur = conn.cursor()
+    get_artist_from_tastedive("SZA", cur, conn)
 
 if __name__ == "__main__":
     main()
